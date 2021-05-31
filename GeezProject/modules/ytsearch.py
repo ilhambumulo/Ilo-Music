bcf784a3e7d811cceb66e64a8cb74678f5d89951
ilhambumulo <84878662+ilhambumulo@users.noreply.github.com>
2021-06-01 00:05:07 +0800

    Update ytsearch.py

--- GeezProject/modules/ytsearch.py
+++ GeezProject/modules/ytsearch.py
@@ -18,10 +18,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("/search masukan judul lagu!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("🔎 **Sedang Mencari lagu...**")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
