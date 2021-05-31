8eb3a2837c651a3c7edb85baf2aa05eed9ec2807
ilhambumulo <84878662+ilhambumulo@users.noreply.github.com>
2021-05-31 18:30:20 +0800

    Update config.py

--- GeezProject/config.py
+++ GeezProject/config.py
@@ -10,17 +10,14 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Vckyouuu")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/ad54dc991d9e41dbbe37d.png")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "@infoiam")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "GeezAssistant")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "VcgSupportGroup")
-PROJECT_NAME = getenv("PROJECT_NAME", "Geez Mudic Project")
-SOURCE_CODE = getenv("SOURCE_CODE", "github.com/Vckyou/Geez-MusicProject")
+
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", "DMXINC-QXTJUY-FRTYXB-DDTSFW-ARQ")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
